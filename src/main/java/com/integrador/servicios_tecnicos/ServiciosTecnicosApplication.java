c61929a707f1fbd6337ed0e7dce5df56b94029fb
Nicolas Altez <nicolasaltezacosta27@gmail.com>
2024-07-30 19:33:49 -0300

    initial commit

---
+++ src/main/java/com/integrador/servicios_tecnicos/ServiciosTecnicosApplication.java
@@ -0,0 +1,13 @@
+package com.integrador.servicios_tecnicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciosTecnicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciosTecnicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/integrador/servicios_tecnicos/ServiciosTecnicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integrador.servicios_tecnicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciosTecnicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
