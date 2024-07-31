95601180017a08ca7178c0755973854eea17e436
Nicolas Altez <nicolasaltezacosta27@gmail.com>
2024-07-31 16:58:44 -0300

    logger application running and banner

--- src/main/java/com/integrador/servicios_tecnicos/ServiciosTecnicosApplication.java
+++ src/main/java/com/integrador/servicios_tecnicos/ServiciosTecnicosApplication.java
@@ -1,13 +1,18 @@
 package com.integrador.servicios_tecnicos;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServiciosTecnicosApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ServiciosTecnicosApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ServiciosTecnicosApplication.class, args);
+		LOGGER.info("Application running");
 	}
 
 }
