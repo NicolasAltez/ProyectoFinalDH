eaf038874b92651cce274a3d3921ed8fd359a64b
FernandoLanza <josefernandolanzabeltran@gmail.com>
2024-08-02 15:56:54 -0500

    Se agrega productRepository y producService

---
+++ src/main/java/com/integrador/servicios_tecnicos/service/impl/ProductService.java
@@ -0,0 +1,14 @@
+
+
+import javax.transaction.Transactional;
+
+import main.java.com.integrador.servicios_tecnicos.service.IProductService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+ 
+public class ProductService implements IProductService {
+    
+}
