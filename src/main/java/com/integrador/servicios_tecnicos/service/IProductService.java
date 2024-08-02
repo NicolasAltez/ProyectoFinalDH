28f7aada48c5fe7117ac6a284e47c85c1764a0dc
FernandoLanza <josefernandolanzabeltran@gmail.com>
2024-08-02 15:56:54 -0500

    Se agrega productRepository y producService

---
+++ src/main/java/com/integrador/servicios_tecnicos/service/IProductService.java
@@ -0,0 +1,7 @@
+package main.java.com.integrador.servicios_tecnicos.service;
+
+import java.util.List;
+
+public interface IProductService {
+
+}
---
+++ src/main/java/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package main.java.repository;
+
+import com.integrador.servicios_tecnicos.models.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public @interface ProductRepository extends JpaRepository<Product, Long >{
+    
+}
