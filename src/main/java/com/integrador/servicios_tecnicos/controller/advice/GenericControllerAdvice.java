76f692904692a4639831e3096bcb7f4812a67343
Nicolas Altez <nicolasaltezacosta27@gmail.com>
2024-08-02 22:02:06 -0300

    [DESA-46-50 : GenericControllerAdvice and handleMethodNotValid exception]

---
+++ src/main/java/com/integrador/servicios_tecnicos/controller/advice/GenericControllerAdvice.java
@@ -0,0 +1,40 @@
+package com.integrador.servicios_tecnicos.controller.advice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.validation.FieldError;
+
+@RestControllerAdvice
+public class GenericControllerAdvice extends ResponseEntityExceptionHandler {
+    private final Logger LOGGER = LoggerFactory.getLogger(GenericControllerAdvice.class);
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        List<String> errors = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        Map<String, Object> responseBody = Map.of(
+                "message", "Validation failed",
+                "errors", errors,
+                "timestamp", LocalDateTime.now(),
+                "status", HttpStatus.BAD_REQUEST
+
+        );
+        LOGGER.error("error trying validate request body: ", ex);
+        return new ResponseEntity<>(responseBody, HttpStatus.BAD_REQUEST);
+    }
+    }
+}
