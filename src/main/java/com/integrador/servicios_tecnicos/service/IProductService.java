b50957e432c744fab28ad20edab119ef416f3795
FernandoLanza <josefernandolanzabeltran@gmail.com>
2024-08-06 00:11:43 -0500

    Implementacion de ProductController con métodos CRUD para manejar operaciones de productos

--- src/main/java/com/integrador/servicios_tecnicos/service/IProductService.java
+++ src/main/java/com/integrador/servicios_tecnicos/service/IProductService.java
@@ -1,5 +1,15 @@
-package com.integrador.servicios_tecnicos.service;
+package main.java.com.integrador.servicios_tecnicos.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.integrador.servicios_tecnicos.models.entity.Product;
 
 public interface IProductService {
-
+    //Dedo crear las dto?
+    Product createProduct(Product product);
+    Product updateProduct(Long id, Product product);
+    void deleteProduct(Long id);
+    Optional<Product> getProductById(Long id);
+    List<Product> getAllProducts();
 }
--- src/main/java/com/integrador/servicios_tecnicos/service/impl/ProductService.java
+++ src/main/java/com/integrador/servicios_tecnicos/service/impl/ProductService.java
@@ -1,8 +1,51 @@
 package com.integrador.servicios_tecnicos.service.impl;
 
+import com.integrador.servicios_tecnicos.models.entity.Product;
+import com.integrador.servicios_tecnicos.repository.ProductRepository;
 import com.integrador.servicios_tecnicos.service.IProductService;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService implements IProductService {
+
+   @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product updateProduct(Long id, Product product) {
+        if (productRepository.existsById(id)) {
+            product.setId(id);
+            return productRepository.save(product);
+        }
+        throw new ProductNotFoundException("Product not found with id: " + id);
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        if (productRepository.existsById(id)) {
+            productRepository.deleteById(id);
+        } else {
+            throw new ProductNotFoundException("Product not found with id: " + id);
+        }
+    }
+
+    @Override
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
 }
