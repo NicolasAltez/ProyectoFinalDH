a52fcea50f3b74e894ff3c1fe02467b7b4006ae0
FernandoLanza <josefernandolanzabeltran@gmail.com>
2024-08-05 22:10:49 -0500

    Correcciones sugeridas

--- src/main/java/repository/ProductRepository.java
+++ src/main/java/repository/ProductRepository.java
@@ -3,6 +3,6 @@ package main.java.repository;
 import com.integrador.servicios_tecnicos.models.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public @interface ProductRepository extends JpaRepository<Product, Long >{
-    
+public interface ProductRepository extends JpaRepository<Product, Long > {
+
 }
