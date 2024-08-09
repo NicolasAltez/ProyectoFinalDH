262843153bca4a4238eac7863bb835c39b3d4f05
Nicolas Altez <nicolasaltezacosta27@gmail.com>
2024-08-09 19:50:48 -0300

    DESA-add-image-url: add image in products dtos

--- src/main/java/com/integrador/servicios_tecnicos/models/dtos/category/CategoryWithName.java
+++ src/main/java/com/integrador/servicios_tecnicos/models/dtos/category/CategoryWithName.java
@@ -12,4 +12,5 @@ import lombok.NoArgsConstructor;
 public class CategoryWithName {
     private Long id;
     private String name;
+    private String urlImage;
 }
--- src/main/java/com/integrador/servicios_tecnicos/models/dtos/products/ProductNameDTO.java
+++ src/main/java/com/integrador/servicios_tecnicos/models/dtos/products/ProductNameDTO.java
@@ -12,4 +12,5 @@ import lombok.NoArgsConstructor;
 public class ProductNameDTO {
     private Long id;
     private String name;
+    private String urlImage;
 }
